14425eb33bed6153b900b82bad05b62987f86a57
Broadwater, Tim [US-US] <tim.broadwater@leidos.com>
2021-02-04 16:03:59 -0500

    Added logo and smooth crolling

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+// Smooth Scrolling for Anchors With Offset
+jQuery(function() {
+  jQuery('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = jQuery(this.hash);
+      target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        jQuery('html,body').animate({
+          scrollTop: target.offset().top -100
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
